2cd31e3a9f9c183be38923e7d8704dfa41b3c958
YoonKyungSoo <edward4992@gmail.com>
2024-10-30 15:42:53 +0900

    헤더 레이아웃 정리

--- KHleague/src/main/java/com/example/demo/controller/myPage/MyPageController.java
+++ KHleague/src/main/java/com/example/demo/controller/myPage/MyPageController.java
@@ -9,7 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyPageController {
 
 	@GetMapping("mypage_profile")
-	public String ShowMyPage() {
+	public String ShowMyPage_profile() {
 		return "mypage/mypage_profile";
 	}
+
+	@GetMapping("mypage_money")
+	public String Showmypage_money() {
+		return "mypage/mypage_money";
+	}
+
+	@GetMapping("mypage_order")
+	public String Showmypage_order() {
+		return "mypage/mypage_order";
+	}
+
+	@GetMapping("mypage_wish")
+	public String ShowMyPage_wish() {
+		return "mypage/mypage_wish";
+	}
+
+	@GetMapping("mypage_buy")
+	public String ShowMyPage_buy() {
+		return "mypage/mypage_buy";
+	}
+
 }
