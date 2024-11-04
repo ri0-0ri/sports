a59accd855d2a7382b7df002a74b6b20d7b929eb
ri0-0ri <vveinerra@gmail.com>
2024-11-04 16:12:43 +0900

    tq

---
+++ KHleague/src/main/java/com/example/demo/model/payment/kakaopayDTO.java
@@ -0,0 +1,9 @@
+package com.example.demo.model.payment;
+
+import lombok.Data;
+
+@Data
+public class kakaopayDTO {
+	private String tid;
+	private String next_redirect_pc_url;
+}
---
+++ KHleague/src/main/java/com/example/demo/service/payment/PayService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.payment;
+
+import java.util.Map;
+
+import com.example.demo.model.payment.kakaopayDTO;
+
+public interface PayService {
+
+//	kakaopayDTO kakaopay(Map<String, Object> params);
+
+}
