d15000d58103a6a3e10acc27973dc881eb322335
YoonKyungSoo <edward4992@gmail.com>
2024-11-05 12:21:37 +0900

    경기일정 추가완료

--- KHleague/src/main/java/com/example/demo/service/gWillBoard/GWillBoardService.java
+++ KHleague/src/main/java/com/example/demo/service/gWillBoard/GWillBoardService.java
@@ -1,8 +1,7 @@
 package com.example.demo.service.gWillBoard;
 
+import com.example.demo.modal.gWillBoardDTO.GWillBoardDTO;
 
 public interface GWillBoardService {
-
-
-
+    void addGWillBoard(GWillBoardDTO gWillBoardDTO); // 일정 추가 메서드
 }
