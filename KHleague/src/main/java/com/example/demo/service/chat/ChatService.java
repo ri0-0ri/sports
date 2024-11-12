e85bc17927a88080b014d57969f57c3581fc418f
YoonKyungSoo <edward4992@gmail.com>
2024-11-12 12:23:25 +0900

    저장은 됨 세션해야함

--- KHleague/src/main/java/com/example/demo/service/chat/ChatService.java
+++ KHleague/src/main/java/com/example/demo/service/chat/ChatService.java
@@ -1,6 +1,21 @@
 package com.example.demo.service.chat;
 
+import com.example.demo.mapper.chat.ChatMapper;
+import com.example.demo.model.chat.ChatDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChatService {
 
+	private final ChatMapper chatMapper;
+
+	@Autowired
+	public ChatService(ChatMapper chatMapper) {
+		this.chatMapper = chatMapper;
+	}
+
+	public void saveChatMessage(ChatDTO chatDTO) {
+		chatMapper.insertChatMessage(chatDTO); // DB에 채팅 메시지 저장
+	}
 }
