9c35c9bda59772e57919d2690c5e5012083749bf
YoonKyungSoo <edward4992@gmail.com>
2024-11-20 12:03:08 +0900

    메인 경기정보 가져오기

--- KHleague/src/main/java/com/example/demo/controller/main/HomeController.java
+++ KHleague/src/main/java/com/example/demo/controller/main/HomeController.java
@@ -1,18 +1,27 @@
 package com.example.demo.controller.main;
 
+import com.example.demo.service.main.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	private HomeService homeService;
+
 	@GetMapping("/")
 	public String Home(HttpSession session, Model model) {
 		String loginUser = (String) session.getAttribute("loginUser");
 		model.addAttribute("isLoggedIn", loginUser != null);
-		return "index";
+
+		// 경기 정보 가져오기
+		model.addAttribute("upcomingMatches", homeService.getUpcomingMatches());
+
+		return "index"; // index.html로 전달
 	}
 
 	@GetMapping("/join/join.html")
@@ -24,5 +33,4 @@ public class HomeController {
 	public String loginPage() {
 		return "login_page/login_page"; // 해당 페이지의 뷰 이름을 반환
 	}
-
 }
